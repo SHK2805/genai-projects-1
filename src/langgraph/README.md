855f855d6823cf92831e59ccef50f2442296aa5a
Sri <30577467+SHK2805@users.noreply.github.com>
2025-03-27 23:03:29 +0000

    adding graph

---
+++ src/langgraph/README.md
@@ -0,0 +1,10 @@
+# LangGraph
+* LangGraph is a library within the LangChain ecosystem designed to simplify the development of complex applications involving multiple large language models (LLMs). 
+* It introduces a graph-based framework for orchestrating workflows, enabling developers to create stateful, multi-agent systems efficiently.
+* Key features of LangGraph include:
+  - **Graph Structures**: Applications are represented as directed graphs, where nodes perform specific tasks (like text generation or data manipulation) and edges define the flow of information.
+  - **State Management**: LangGraph automatically tracks and updates the state across interactions, ensuring context is maintained.
+  - **Coordination**: It ensures agents execute tasks in the correct order and exchange information seamlessly.
+* LangGraph is particularly useful for building sophisticated conversational flows, integrating tools, and managing dynamic execution paths.
+* [Tutorial](https://blog.futuresmart.ai/langgraph-tutorial-for-beginners)
+* [Tutorial](https://www.datacamp.com/tutorial/langgraph-tutorial)
--- src/langgraph/simple_graph.py
+++ src/langgraph/simple_graph.py
@@ -3,4 +3,50 @@ from typing_extensions import TypedDict
 class State(TypedDict):
     graph_state: str
 
+class FirstNode:
+    def __init__(self, node_state: State):
+        self.name = self.__class__.__name__
+        self.state = node_state
 
+    def print_state(self):
+        print(f"Node Name: {self.name}")
+        print(f"Node State: {self.state['graph_state']}")
+
+    def get(self):
+        return self.state['graph_state'] + " I am playing"
+
+    def set(self, state: str):
+        self.state['graph_state'] = state
+        print(f"State set to: {self.state['graph_state']}")
+
+class SecondNode:
+    def __init__(self, node_state: State):
+        self.name = self.__class__.__name__
+        self.state = node_state
+
+    def print_state(self):
+        print(f"Node Name: {self.name}")
+        print(f"Node State: {self.state['graph_state']}")
+
+    def get(self):
+        return self.state['graph_state'] + " Cricket"
+
+    def set(self, state: str):
+        self.state['graph_state'] = state
+        print(f"State set to: {self.state['graph_state']}")
+
+class ThirdNode:
+    def __init__(self, node_state: State):
+        self.name = self.__class__.__name__
+        self.state = node_state
+
+    def print_state(self):
+        print(f"Node Name: {self.name}")
+        print(f"Node State: {self.state['graph_state']}")
+
+    def get(self):
+        return self.state['graph_state'] + " Tennis"
+
+    def set(self, state: str):
+        self.state['graph_state'] = state
+        print(f"State set to: {self.state['graph_state']}")
